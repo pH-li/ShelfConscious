a2614f7a9597e43c16dbd907cdc4fdca0fae5a03
Lily Phan <79292581+pH-li@users.noreply.github.com>
2025-01-11 17:59:28 -0500

    Updating the identifyFood file

--- identifyFood.py
+++ identifyFood.py
@@ -5,43 +5,50 @@ All the data associated with identifying what the user is uploading.
 # Import statements
 import cv2
 import streamlit as st
+import datetime
 
+st.title('ShelfConcious')
+st.write('Welcome! This page is made for you to take or upload pictures of your food. \n'
+         'Please make sure to take a clear picture of a single food item you wish to log.'
+         ' Press either of the two options below.')
 
-class IdentifyFood:
-    """
-    This class is intended to allow a user to upload or take a picture,
-    which will be identified by our program.
-    """
+# Initialize session state
+if 'frame' not in st.session_state:
+    st.session_state.frame = None
+if 'picture_taken' not in st.session_state:
+    st.session_state.picture_taken = False
 
-    def __init__(self, *args, **kwargs) -> None:
-        st.title('My First Streamlit App')
-        st.write('Hello, World!')
+left_spacer, col1, col2, right_spacer = st.columns([1, 1, 1, 1])
 
-    def takePicture(self) -> None:
-        """
-        This method is responsible just for taking the image.
-        """
-        # Initialize the camera
-        cap = cv2.VideoCapture(0)
+with col1:
+    takePicture = st.button(label='Take a Picture')
 
-        # Check if the camera opened successfully
-        if not cap.isOpened():
-            print("Error: Could not open camera.")
-            exit()
+with col2:
+    st.button(label='Upload a Picture')
 
-        # Capture a single frame
-        ret, frame = cap.read()
+if takePicture:
+    cap = cv2.VideoCapture(0)
+    ret, frame = cap.read()
+    cap.release()
 
-        if ret:
-            # Save the captured frame as an image
-            cv2.imwrite('captured_image.jpg', frame)
-            print("Image captured successfully!")
+    if ret:
+        st.session_state.frame = frame
+        st.session_state.picture_taken = True
+        st.image(frame, channels="BGR")
+    else:
+        st.error("Failed to capture image. Please try again.")
+
+left_spacer1, col3, right_spacer1 = st.columns([1, 1, 1])
+
+if st.session_state.picture_taken:
+    with col3:
+        st.write("Please press \"Take a Picture\" again to retake. Press confirm picture to save.")
+        confirmation = st.button(label='Confirm Picture')
+
+    if confirmation:
+        if st.session_state.frame is not None:
+            cv2.imwrite('images/a.png', st.session_state.frame)
+            st.success("Image saved successfully!")
+            st.session_state.picture_taken = False  # Reset the state
         else:
-            print("Error: Could not capture image.")
-
-        # Release the camera
-        cap.release()
-
-
-if __name__ == '__main__':
-    temp = IdentifyFood()
+            st.error("No image to save. Please take a picture first.")

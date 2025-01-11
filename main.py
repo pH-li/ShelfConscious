f301c38e72010bddb85531f4ab9d5a95a44dd9af
Yiping Chen <yping.chen@mail.utoronto.ca>
2025-01-11 17:37:03 -0500

    bg + pages

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 # TEMP
 
 # FrontEnd
+# Log-in
 import streamlit as sl
+
+background_image = """
+<style>
+[data-testid="stAppViewContainer"] {background-color: #ffffff;
+opacity: 1;
+background: linear-gradient(135deg, #fffbfc55 25%, transparent 25%) -17px 0/ 34px 34px, linear-gradient(225deg, #fffbfc 25%, transparent 25%) -17px 0/ 34px 34px, linear-gradient(315deg, #fffbfc55 25%, transparent 25%) 0px 0/ 34px 34px, linear-gradient(45deg, #fffbfc 25%, #ffffff 25%) 0px 0/ 34px 34px;
+}
+</style>
+"""
+sl.markdown(
+    background_image, unsafe_allow_html=True)
+
+
 sl.write("Log in")
-sl.text_input("Username")
-sl.text_input("Password")
+username = sl.text_input("Username")
+password = sl.text_input("Password")
+if sl.button("Go"):
+    sl.switch_page("pages/1_displayoptions.py")

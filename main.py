a7f8de134cf35935c403227e9dabb1fa76f07ab5
Paige Li <paigeli@Paiges-MacBook-Pro.local>
2025-01-11 15:54:25 -0500

    asdfadf

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 # TEMP
 
-# FrontEnd
-import streamlit as sl
-sl.write("Log in")
-sl.text_input("Username")
-sl.text_input("Password")
+import streamlit as st
+st.write("hellow world")
+#st.text_input("Enter text") #returns value that user puts in
+#can store it into a variable and use it:
+text=st.text_input("Enter text")
+st.write(f"the text you entered is: {text}")
+
+#button
+is_clicked=st.button("Click me")
+
+#title
+st.write("## This is a H2 title!")
+
+st.link_button("name of button", "url to page")
